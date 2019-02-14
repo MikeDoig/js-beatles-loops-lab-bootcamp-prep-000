876f2306f478f3cd996709c7cd7e3f49ee6ce5ca
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 19:34:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,8 +26,3 @@ function iLoveTheBeatles(number) {
   while (number<15);
   return num;
 }
-  const array[];
-}
-for (let i=0, l = musicians.length; i<l; i++) {
-  return `${musicians} plays ${instrument}`;
-  }
