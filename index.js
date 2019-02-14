6e7f1d08ea15a96829995480dcf2793117c1dadc
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 19:32:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
+<<<<<<< HEAD
   const array = [];
 
 for (let i=0, l = musicians.length; i<l; i++) {
@@ -26,3 +27,10 @@ function iLoveTheBeatles(number) {
   while (number<15);
   return num;
 }
+=======
+  const array[]
+}
+for (let i=0, l = musicians.length; i<l; i++) {
+  return `${musicians} plays ${instrument}`;
+  }
+>>>>>>> e5dd6901679392ec35b4a94969cb3685081f97d9
